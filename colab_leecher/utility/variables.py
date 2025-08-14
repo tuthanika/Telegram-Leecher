221234567918a196a71b028a2eef381d7a485dd9
tuthanika <tuthanika@gmail.com>
2025-08-14 16:00:26 +0700

    thay server ảnh tránh lỗi vặt

--- colab_leecher/utility/variables.py
+++ colab_leecher/utility/variables.py
@@ -105,7 +105,7 @@ class MSG:
 
 class Aria2c:
     link_info = False
-    pic_dwn_url = "https://picsum.photos/900/600"
+    pic_dwn_url = "https://random-image-pepebigotes.vercel.app/api/random-image"
 
 
 class Gdrive:
